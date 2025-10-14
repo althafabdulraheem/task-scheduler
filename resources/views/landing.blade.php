a36004b7d8f4890ed194ef4bf2ec6994917afe10
althafabdulraheem <althafbinraheem786@gmail.com>
2025-10-14 21:57:04 +0400

    resolved the modal close issue

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -69,7 +69,7 @@
     <div class="modal-content">
       <div class="modal-header">
         <h5 class="modal-title">Modal title</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+        <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
             
           <span aria-hidden="true">x</span>
         </button>
@@ -79,7 +79,7 @@
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-primary">Save changes</button>
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
       </div>
     </div>
   </div>
